7c2e87de775beaf91502864b9408add8fe399a55
Mike Kroutikov <mkroutikov@innodata.com>
2018-11-09 16:22:25 -0500

    streaming reading is back!

--- tbert/optimization.py
+++ tbert/optimization.py
@@ -6,7 +6,7 @@ class LineraDecayWithWarpupLR(LambdaLR):
     def __init__(self, optimizer, train_steps, warmup_steps, last_epoch=-1):
 
         def schedule(step):
-            if stem <= warmup_steps:
+            if step <= warmup_steps:
                 return step / warmup_steps
             assert step <= train_steps
             return 1. - (train_steps - step) / (train_steps - warmup_steps)
