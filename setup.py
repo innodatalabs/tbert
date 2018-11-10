328d82696c93660bc3299ee88ef7552a77b12813
Mike Kroutikov <mkroutikov@innodata.com>
2018-11-09 19:03:30 -0500

    preparing for pypi

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from tbert import __version__, __description__, __url__, __author__, \
+    __author_email__, __keywords__
+
+NAME = 'tbert'
+
+setup(
+    name=NAME,
+    version=__version__,
+    description=__description__,
+    long_description='See ' + __url__,
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    keywords=__keywords__,
+
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    packages=[NAME],
+    install_requires=['torch~=4.2.0'],
+    entry_points={
+        'console_scripts': [
+            'ilabs_bulk_predict=ilabs.client.ilabs_bulk_predict:main',
+            'ilabs_dv=ilabs.client.ilabs_datavault_api:main'
+        ]
+    }
+)
---
+++ tbert/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.0'
+__description__ = 'BERT Neural Net module for pytorch'
+__url__ = 'https://github.com/innodatalabs/tbert'
+__author__ = 'Mike Kroutikov'
+__author_email__ = 'mkroutikov@innodata.com'
+__keywords__ = ['nlp', 'neural networks', 'pytorch', 'tensorflow']
